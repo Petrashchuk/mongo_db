16f1e802c93d26bfd49783cccf0b5dc0bc0fb554
Andrii Loza <loza.andriy7@gmail.com>
2020-04-24 18:51:06 +0300

    add todos

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-
 app.use(require('./routes'));
 
-
+//todo run seed
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
--- controllers/Users.js
+++ controllers/Users.js
@@ -1,17 +1,21 @@
 const MongoClient = require('../connect_to_DB');
 const userRandom = require('../Randomizer/Users.randomizer');
 
-
 module.exports.create_user = function (req, res) {
     // const {age, gender, nationality} = req.body;
+    //todo initialize outside the function
     const collection = MongoClient.db('DataBase').collection('users');
+    //todo rename collections => userCollections
+
     collection.insertMany(userRandom).then(res => {
         console.log(res);
     });
 };
 
 module.exports.remove_all_users = function (req, res) {
+    //todo initialize outside the function
     const collection = MongoClient.db('DataBase').collection('users');
+    //todo rename collections => userCollections
     collection.remove().then(res => {
         console.log(res);
     });
