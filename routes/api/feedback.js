539a7050d49a78c5a695417d6aa4accaf075418d
loza.andriy7 <loza.andriy7@gmail.com>
2020-05-26 17:42:16 +0300

    added a procfile

--- routes/api/feedback.js
+++ routes/api/feedback.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
-// const auth = require('../auth');
-
 
 const feedbackController = require('../../controllers/feedbacks.controller');
 
-router.post('/create_feedback', feedbackController.create_feedback);
-router.delete('/remove_feedback', feedbackController.remove_feedbacks);
 router.get('/show_statistic', feedbackController.show_statistic);
 
 module.exports = router;
